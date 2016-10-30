a78996a634c229bb50062606520c1069372fb5eb
Minhao Cheng <cmhcbb@gmail.com>
2016-10-29 20:33:50 -0700

    some explaination

--- synthesisDataGen.m
+++ synthesisDataGen.m
@@ -1,6 +1,6 @@
-m=100;
+m=100;  %number of users
 C=1;
-X=randn(64,100);
+X=randn(64,100);  % d=64 #movies=100
 d=size(X,1);
 k=5; %k is the rank
 %u=(-1*ones(d,k)+2*rand(d,k))/10;
@@ -14,14 +14,14 @@ train={};
 for i=1:100
     realw=realpara(:,i);
     rating=realw'*X;
-    sampleindex=randi(100,15,1);
+    sampleindex=randi(100,15,1);     % each user has 15 ratings
     usercol=ones(15,1)*i;
     samplecol=sampleindex;
     ratecol=rating(sampleindex)';
     train{i}=[usercol,samplecol,ratecol];
 end
 train=train';
-testset=genTestset(train,5);        % 5 per user as the testset
+testset=genTestset(train,5);        % 5 ratings per user as the testset
 testset= arrayfun(@(x) testset(testset(:,1)==x,:),unique(testset(:,1)),'uniformoutput',false);
 syngenpair;
 ranksvmgrad;
