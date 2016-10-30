fd4e4535b18e2c4f0c7c5de042158b505f60db4e
Minhao Cheng <cmhcbb@gmail.com>
2016-10-30 11:05:55 -0700

    Update rank1test.m

--- rank1test.m
+++ rank1test.m
@@ -1,18 +1,15 @@
 function acc=rank1test(w,testset,X,m)
     rcount=0;
     totalcount=0;
-    for i=1:m
-        I=find(testset(:,1)==i);
-        if (size(I,1)<=1)
-            continue;
-        end
-        for j=1:size(I,1)
-            for k=j+1:size(I,1)
-                res=w'*(X(:,testset(I(j),2))-X(:,testset(I(k),2)));
-                real=testset(I(j),3)-testset(I(k),3);
+    for i=1:size(testset,1)
+        testmat=testset{i};
+        for j=1:size(testmat,1)
+            for k=j+1:size(testmat,1)
+                real=testmat(j,3)-testmat(k,3);
                 if real==0
                     continue;
                 end
+                res=w'*(X(:,testmat(j,2))-X(:,testmat(k,2)));
                 totalcount=totalcount+1;
                 if res*real>0
                     rcount=rcount+1; 
@@ -20,4 +17,4 @@ function acc=rank1test(w,testset,X,m)
             end
         end
     end
-    acc=rcount/totalcount;
+    %acc=rcount/totalcount;
