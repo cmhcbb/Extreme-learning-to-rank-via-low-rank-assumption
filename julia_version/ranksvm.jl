a5c8459dc11284a4376c6df9628a6e496f3dc0f7
Minhao Cheng <cmhcbb@gmail.com>
2016-11-02 10:51:38 -0700

    worked rank1svm

--- julia_version/ranksvm.jl
+++ julia_version/ranksvm.jl
@@ -1,17 +1,17 @@
 function ranksvm(X,train,testset,m,deltax,paridx)
 	C=1
 	d=size(X,1)
-	w=rand(d,1)
-	res=deltax'*w
-	loss=0
+	w=randn(d,1)
 	for iter=1:200
+		loss=0
 		println("Iteration=",iter)
 		deltaw=zeros(d,1)
+		res=deltax'*w
 		for i=1:size(res,1)
-			loss+=max(0,1-res[i])
-			deltaw+=max(0,1-res[i])*deltax[i]	
+			loss+=max(0,1-res[i,1])
+			deltaw+=max(0,1-res[i,1])*deltax[:,i]	
 		end
-	eta=1e-8
+	eta=1e-5
 	gradf=w-2*C*deltaw
 	w=w-eta*gradf
 	funv=0.5*norm(w)+C*loss*loss
